7e9b2232311eb2fc04172e39689af6b158be4315
Perry Sittser <sittserp@gmail.com>
2020-10-27 14:22:07 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <div className="image-list">
+        <div className="image-item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
